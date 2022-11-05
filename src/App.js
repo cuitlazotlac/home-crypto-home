7b44193abbb31552ae088ec927e28eec7cfa0f91
cuitlazotlac <cuitlazotlac@gmail.com>
2022-11-04 23:19:22 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Home Crypto Home
         </p>
         <a
           className="App-link"
